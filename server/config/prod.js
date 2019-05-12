fe84b31905a8169b59e88866808ec012558927e1
Philippe <phildoreau45@gmail.com>
2019-05-12 15:37:01 +0200

    test deployment

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
   DB_URI: process.env.DB_URI,
+  NAMESPACE: 'https://philippedoreau.herokuapp.com',
   SENDGRID_API: process.env.SENDGRID_API,
-  NAMESPACE: 'https://philippedoreau.herokuapp.com'
 };
