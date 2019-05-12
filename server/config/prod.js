b46836c45751c55612b9f6b4e3ad81199aeb4e76
Philippe <phildoreau45@gmail.com>
2019-05-12 10:17:09 +0200

    Pre-deploy setup

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,4 @@
 module.exports = {
   //DB_URI: process.env.DB_URI,
-  DB_URI: 'mongodb+srv://pdoreau:Ymx43yq5siGaRGXk@cluster0-qplcj.mongodb.net/portfolio-doreau?retryWrites=true',
   SENDGRID_API: process.env.SENDGRID_API
 };
