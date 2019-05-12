625a66bede1597c4c7c90e4b8819df3b12c2276b
Philippe <phildoreau45@gmail.com>
2019-05-12 15:03:31 +0200

    namespace

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
   DB_URI: process.env.DB_URI,
   SENDGRID_API: process.env.SENDGRID_API,
-  NAMESPACE: 'https://philippedoreau.herokuapp.com/'
+  NAMESPACE: 'https://philippedoreau.herokuapp.com'
 };
